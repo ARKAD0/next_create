2b91bdb75466b25c0ed807f6be70d963fff0a752
Arkad0 <danielcardenasparra@gmail.com>
2022-06-13 03:26:34 -0500

    agregando button

---
+++ components/Button.js
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
+export function ButtonContact (){
+    return (
+    <Button
+    bg={'blue.300'}
+    size='md'
+    color='white'
+  >
+    Contáctanos <a href="./"></a>
+  </Button>)
+}
